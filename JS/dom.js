44065dcf64a306acdbb504972e2ff4db199e6a81
mo7amedehab97 <gazacityface@gmail.com>
2022-02-09 20:22:09 +0200

    add the show functionality and save the products to the local relates #39

--- JS/dom.js
+++ JS/dom.js
@@ -28,23 +28,13 @@ addItem.addEventListener("click", (e) => {
     categoryInput.value &&
     description.value !== ""
   ) {
-    result = addData([], items);
+      result = addData([], items);
   }
-  addToLocalstorage(result, items);
-  window.location.reload();
-  console.log(result);
+    addToLocalstorage(result, items);
+    let carda = JSON.parse(localStorage.getItem("cardsArray"));
+    window.location.reload();    
 });
 
-// Toggle products view (grid | list)
-gridView.onclick = () => {
-  products.classList.remove("list");
-  products.classList.add("grid");
-};
-
-listView.onclick = () => {
-  products.classList.remove("grid");
-  products.classList.add("list");
-};
 
 // Display Produtcs
 
@@ -97,4 +87,66 @@ function renderProducts(cardsArray) {
   }
 }
 
-renderProducts(cardsArray);
+// renderProducts(cardsArray);
+
+
+function showCards(cardsArray) {
+  if (cardsArray != null) {
+    cardsArray.forEach((item) => {
+      const product = document.createElement("div");
+      product.className = "product";
+
+      const img = document.createElement("img");
+      img.className = "product-img";
+      img.src = item.image;
+      img.alt = "Image";
+
+      const details = document.createElement("div");
+      details.className = "details";
+
+      const productName = document.createElement("h4");
+      productName.className = "product-name";
+      productName.textContent = item.name;
+
+      const productDesc = document.createElement("p");
+      productDesc.className = "product-desc";
+      productDesc.textContent = item.description;
+
+      const footCard = document.createElement("div");
+      footCard.className = "foot-card";
+
+      const productPrice = document.createElement("h5");
+      productPrice.className = "product-price";
+      productPrice.textContent = item.price;
+
+        const trash = document.createElement("i")
+      const pencil = document.createElement("i");
+            ;
+        trash.className = "fas fa-trash-can";
+        pencil.className = "fa-solid fa-pencil";
+        
+      trash.setAttribute(
+        "onclick",
+        `deletecard(${cardsArray}, ${Number(item.id)})`
+      );
+      products.appendChild(product);
+
+      product.appendChild(img);
+      product.appendChild(details);
+
+      details.appendChild(productName);
+      details.appendChild(productDesc);
+      details.appendChild(footCard);
+
+      footCard.appendChild(productPrice);
+        footCard.appendChild(trash);
+      footCard.appendChild(pencil);
+        
+    });
+  }
+   
+}
+
+
+
+showCards(cardsArray);
