2c1b795282f4bba18b445b8749438bdaeb21d5bf
mo7amedehab97 <gazacityface@gmail.com>
2022-02-09 12:29:26 +0200

    create test for the search function relates #29

--- tests.test.js
+++ tests.test.js
@@ -1,4 +1,4 @@
-const { addData } = require("./JS/logic.js");
+const { addData, searchByName } = require("./JS/logic.js");
 
 describe("add data to array", () => {
   test("test add data to the array products", () => {
@@ -7,6 +7,21 @@ describe("add data to array", () => {
       { obj: "3", ob: "2" },
       { ore: "2", ofg: "3" },
     ];
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("search products via name", () => {
+  test("search the the product via name", () => {
+    const actual = searchByName(
+      [
+        { id: 1, name: "fruit" },
+        { id: 2, name: "salad" },
+        { id: 3, name: "vegatables" },
+      ],
+      "salad"
+    );
+    const expected = [{ id: 2, name: "salad" }];
     expect(actual).toEqual(expected);
   });
 });
