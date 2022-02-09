12464bd7c91e0ead295174a64419dc34d0594a71
nadabassam <nada.b.abu.zaid@gmail.com>
2022-02-09 15:13:53 +0200

    Function to filter products by price Relates #40

--- JS/logic.js
+++ JS/logic.js
@@ -1,13 +1,22 @@
 function addData(arr, obj) {
-  return [...arr, obj];
+    return [...arr, obj];
 }
 // search function
-function searchByName(array,string) {
-   for (let i = 0; i < array.length; i++) {
-     if (array[i].name.includes(string)) {
-       return [array[i]];
-     }
-   }
+function searchByName(array, string) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].name.includes(string)) {
+            return [array[i]];
+        }
+    }
 }
- 
-module.exports = { addData, searchByName};
+
+// filter Function
+const filterProduct = (price = "", products) => {
+    const newProducts = [...products];
+    if (price === "ASC")
+        return newProducts.sort((a, b) => a.price - b.price); // sort price to low
+    if (price === "DESC")
+        return newProducts.sort((a, b) => b.price - a.price); // sort price to High
+};
+
+module.exports = { addData, searchByName, filterProduct };
--- tests.test.js
+++ tests.test.js
@@ -1,27 +1,67 @@
-const { addData, searchByName } = require("./JS/logic.js");
+const { addData, searchByName, filterProduct } = require("./JS/logic.js");
 
 describe("add data to array", () => {
-  test("test add data to the array products", () => {
-    const actual = addData([{ obj: "3", ob: "2" }], { ore: "2", ofg: "3" });
-    const expected = [
-      { obj: "3", ob: "2" },
-      { ore: "2", ofg: "3" },
-    ];
-    expect(actual).toEqual(expected);
-  });
+    test("test add data to the array products", () => {
+        const actual = addData([{ obj: "3", ob: "2" }], { ore: "2", ofg: "3" });
+        const expected = [
+            { obj: "3", ob: "2" },
+            { ore: "2", ofg: "3" },
+        ];
+        expect(actual).toEqual(expected);
+    });
 });
 
 describe("search products via name", () => {
-  test("search the the product via name", () => {
-    const actual = searchByName(
-      [
-        { id: 1, name: "fruit" },
-        { id: 2, name: "salad" },
-        { id: 3, name: "vegatables" },
-      ],
-      "salad"
-    );
-    const expected = [{ id: 2, name: "salad" }];
-    expect(actual).toEqual(expected);
-  });
+    test("search the the product via name", () => {
+        const actual = searchByName(
+            [
+                { id: 1, name: "fruit" },
+                { id: 2, name: "salad" },
+                { id: 3, name: "vegatables" },
+            ],
+            "salad"
+        );
+        const expected = [{ id: 2, name: "salad" }];
+        expect(actual).toEqual(expected);
+    });
 });
+
+describe("Filter products via price and catagory", () => {
+    test("Should return items when filtered by catagory and price", () => {
+        const actual = filterProduct("ASC", [{
+                id: 1,
+                name: "Strawberry",
+                price: 150,
+                image: "https://i.ibb.co/PzGBsgZ/product-12.jpg",
+                category: "Fruits",
+                description: "Lorem ipsum dolor sit amet consct, etur adipiscing elit Nunc vulputate etur adipiscing elit Nunc vulputate.",
+            },
+            {
+                id: 5,
+                name: "Strawberry",
+                price: 200,
+                image: "https://i.ibb.co/PzGBsgZ/product-12.jpg",
+                category: "Fruits",
+                description: "Lorem ipsum dolor sit amet consct, etur adipiscing elit Nunc vulputate etur adipiscing elit Nunc vulputate.",
+            }
+        ]);
+        const expected = [{
+                id: 1,
+                name: "Strawberry",
+                price: 150,
+                image: "https://i.ibb.co/PzGBsgZ/product-12.jpg",
+                category: "Fruits",
+                description: "Lorem ipsum dolor sit amet consct, etur adipiscing elit Nunc vulputate etur adipiscing elit Nunc vulputate.",
+            },
+            {
+                id: 5,
+                name: "Strawberry",
+                price: 200,
+                image: "https://i.ibb.co/PzGBsgZ/product-12.jpg",
+                category: "Fruits",
+                description: "Lorem ipsum dolor sit amet consct, etur adipiscing elit Nunc vulputate etur adipiscing elit Nunc vulputate.",
+            }
+        ];
+        expect(actual).toEqual(expected);
+    });
+})
