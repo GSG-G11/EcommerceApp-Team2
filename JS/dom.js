8bbb4ea5872dd567068a5124ee307bc48173b06e
nadabassam <nada.b.abu.zaid@gmail.com>
2022-02-09 16:03:41 +0200

    Merge convert function and if condition in one function called renderProducts

--- JS/dom.js
+++ JS/dom.js
@@ -16,53 +16,54 @@ listView.onclick = () => {
 // Display Produtcs
 
 let cardsArray = JSON.parse(localStorage.getItem('cardsArray'));
-if (cardsArray != null) {
-    let productsUI = cardsArray.map((item) => {
-        return convert(item);
-    });
+
+function renderProducts(cardsArray) {
+    if (cardsArray != null) {
+        cardsArray.forEach((item) => {
+            const product = document.createElement('div');
+            product.className = "product";
+
+            const img = document.createElement('img');
+            img.className = "product-img";
+            img.src = item.image;
+            img.alt = "Image";
+
+            const details = document.createElement('div');
+            details.className = "details";
+
+            const productName = document.createElement('h4');
+            productName.className = "product-name";
+            productName.textContent = item.name;
+
+            const productDesc = document.createElement('p');
+            productDesc.className = "product-desc";
+            productDesc.textContent = item.description;
+
+
+            const footCard = document.createElement('div');
+            footCard.className = "foot-card";
+
+            const productPrice = document.createElement('h5');
+            productPrice.className = "product-price";
+            productPrice.textContent = item.price;
+
+
+            const cartIcon = document.createElement('i');
+            cartIcon.className = "fas fa-shopping-cart";
+            cartIcon.setAttribute('onclick', `addToCart(${item.id})`);
+            products.appendChild(product);
+
+            product.appendChild(img);
+            product.appendChild(details);
+
+            details.appendChild(productName);
+            details.appendChild(productDesc);
+            details.appendChild(footCard);
+
+            footCard.appendChild(productPrice);
+            footCard.appendChild(cartIcon);
+        });
+    }
 }
 
-function convert(item) {
-    const product = document.createElement('div');
-    product.className = "product";
-
-    const img = document.createElement('img');
-    img.className = "product-img";
-    img.src = item.image;
-    img.alt = "Image";
-
-    const details = document.createElement('div');
-    details.className = "details";
-
-    const productName = document.createElement('h4');
-    productName.className = "product-name";
-    productName.textContent = item.name;
-
-    const productDesc = document.createElement('p');
-    productDesc.className = "product-desc";
-    productDesc.textContent = item.description;
-
-
-    const footCard = document.createElement('div');
-    footCard.className = "foot-card";
-
-    const productPrice = document.createElement('h5');
-    productPrice.className = "product-price";
-    productPrice.textContent = item.price;
-
-
-    const cartIcon = document.createElement('i');
-    cartIcon.className = "fas fa-shopping-cart";
-    cartIcon.setAttribute('onclick', `addToCart(${item.id})`);
-    products.appendChild(product);
-
-    product.appendChild(img);
-    product.appendChild(details);
-
-    details.appendChild(productName);
-    details.appendChild(productDesc);
-    details.appendChild(footCard);
-
-    footCard.appendChild(productPrice);
-    footCard.appendChild(cartIcon);
-}
+renderProducts(cardsArray);
