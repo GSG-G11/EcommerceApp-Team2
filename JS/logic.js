1d1b029a25f3e69ed26f43a4981d78e0f92b9e17
nadabassam <nada.b.abu.zaid@gmail.com>
2022-02-09 17:15:19 +0200

    Solve Error in seller page

--- JS/logic.js
+++ JS/logic.js
@@ -1,20 +1,18 @@
 function addData(arr, obj) {
-  return [...arr, obj];
+    return [...arr, obj];
 }
 // search function
-function searchByName(array,string) {
-   for (let i = 0; i < array.length; i++) {
-     if (array[i].name.includes(string)) {
-       return [array[i]];
-     }
-   }
-}
- 
-// craete function to delete the card 
-function deleteCard(arr, id) {
-  return arr.filter((item) => {
-    return item.id !== id
-  })
+function searchByName(array, string) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].name.includes(string)) {
+            return [array[i]];
+        }
+    }
 }
 
-module.exports = { addData, searchByName,deleteCard};
+// craete function to delete the card 
+function deleteCard(arr, id) {
+    return arr.filter((item) => {
+        return item.id !== id
+    })
+}
