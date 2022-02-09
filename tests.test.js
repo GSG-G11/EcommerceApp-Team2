a572a21ca9275877ddf35c3afa7140294b884eb7
mo7amedehab97 <gazacityface@gmail.com>
2022-02-09 14:35:32 +0200

    create test for the editCard function relates #36

--- tests.test.js
+++ tests.test.js
@@ -1,4 +1,4 @@
-const { addData, searchByName } = require("./JS/logic.js");
+const { addData, searchByName, editCard } = require("./JS/logic.js");
 
 describe("add data to array", () => {
   test("test add data to the array products", () => {
@@ -25,3 +25,56 @@ describe("search products via name", () => {
     expect(actual).toEqual(expected);
   });
 });
+
+describe("add data to array", () => {
+  test("test add data to the array products", () => {
+    const actual = addData([{ obj: "3", ob: "2" }], { ore: "2", ofg: "3" });
+    const expected = [
+      { obj: "3", ob: "2" },
+      { ore: "2", ofg: "3" },
+    ];
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("Should Edit Item value", () => {
+  test("Should Edit Item value", () => {
+    const actual = editCard("1", "price", 50.0, [
+      {
+        id: 0,
+        category: "electronics",
+        price: 30.0,
+        productName: "HP Laptop",
+        image: "../assets/images/laptop.jpg",
+      },
+      {
+        id: 1,
+        category: "food",
+        price: 100.0,
+        productName: "Hamburger",
+        image: "../assets/images/hamburger.jpeg",
+      },
+    ]);
+    const expected = [
+      {
+        id: 0,
+        category: "electronics",
+        price: 30.0,
+        productName: "HP Laptop",
+        image: "../assets/images/laptop.jpg",
+      },
+      {
+        id: 1,
+        category: "food",
+        price: 50.0,
+        productName: "Hamburger",
+        image: "../assets/images/hamburger.jpeg",
+      },
+    ];
+    expect(actual).toEqual(expected);
+  });
+});
+
+
+
+
